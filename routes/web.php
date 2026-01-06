006eb9d408813662330dc4c9e32dc6a9d7ef0963
JianFGO <JianLin.L.He@student.shu.ac.uk>
2026-01-06 12:56:11 +0000

    Add routes for complaint submission

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,17 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ComplaintController;
 
+
+// Default Laravel welcome page
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Submit Complaint (Public)
+Route::get('/complaints/create', [ComplaintController::class, 'create'])
+    ->name('complaints.create');
+
+Route::post('/complaints', [ComplaintController::class, 'store'])
+    ->name('complaints.store');
